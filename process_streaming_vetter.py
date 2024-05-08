33b5600a0cb1de702f0dd3f9d4cac35d42695b22
VetterNic2 <167367275+VetterNic2@users.noreply.github.com>
2024-05-08 15:34:10 -0500

    Added enhancements and updated markdown

--- process_streaming_vetter.py
+++ process_streaming_vetter.py
@@ -1,4 +1,4 @@
-#Imports
+#Imports - Nic Vetter
 
 import csv
 import socket
@@ -16,13 +16,18 @@ OUTPUT_FILE_NAME = "out9.txt"
 def prepare_message_from_row(row):
     return f"{','.join(row)}\n".encode()
 
-# Function to stream data
-def stream_data(input_file_name, output_file_name, address_tuple):
+# Function to stream data 
+# No logging needed because I am writing the streamed data to the output file
+def stream_row(input_file_name, output_file_name, address_tuple):
     with open(input_file_name, "r") as input_file, open(output_file_name, "w") as output_file:
         reader = csv.reader(input_file)
+        #Creating a csv reader. I am also going to account for the header and skip it
         header = next(reader)
+        #separating the attributes by comma's and adding new lines
         output_file.write(','.join(header) + '\n')
-
+        
+        # Using my Contructor to make a socket obj
+        # assigning variable name "sock_object"
         sock_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         for row in reader:
@@ -30,11 +35,11 @@ def stream_data(input_file_name, output_file_name, address_tuple):
             sock_object.sendto(message, address_tuple)
             output_file.write(','.join(row) + '\n')
             time.sleep(random.uniform(1, 3))  
-
+# Calling the stream row function
 if __name__ == "__main__":
     try:
-        print("Starting data streaming.")
-        stream_data(INPUT_FILE_NAME, OUTPUT_FILE_NAME, ADDRESS_TUPLE)
-        print("Streaming complete!")
+        print("Data Currently Streaming.")
+        stream_row(INPUT_FILE_NAME, OUTPUT_FILE_NAME, ADDRESS_TUPLE)
+        print("Stream Complete!")
     except Exception as e:
         print(f"An error occurred: {e}")
